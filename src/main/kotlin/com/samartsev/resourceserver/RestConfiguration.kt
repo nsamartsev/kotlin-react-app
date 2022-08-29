9d5d53f879a58d72013cc8aded251878e8abb26d
Nikita Samartsev <NSSamartsev@sberbank.ru>
2022-08-30 00:55:17 +0300

    add server-side

---
+++ src/main/kotlin/com/samartsev/resourceserver/RestConfiguration.kt
@@ -0,0 +1,14 @@
+package com.samartsev.resourceserver
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+@Configuration
+open class RestConfiguration : RepositoryRestConfigurer {
+    override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration?, cors: CorsRegistry?) {
+        config?.exposeIdsFor(CoffeeShopModel::class.java)
+        config?.setBasePath("/api");
+    }
+}
